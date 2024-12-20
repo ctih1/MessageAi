8d00752ccc592d360929df70b61791687539c272
Onni Nevala <nevalaonni@gmail.com>
2024-12-20 23:33:08 +0200

    added startup scripts

---
+++ run.bat
@@ -0,0 +1,2 @@
+pip install -r requirements.txt
+python .src\message_ai_nevalaonni\__init__.py
---
+++ run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+pip3 install -r requirements.txt
+python3 src/message_ai_nevalaonni/__init__.py
